d3e64144ef4ed91f9c2b0c5c43f99240b6de6c40
LevyLii <782712046@163.com>
2022-09-07 19:49:46 +0800

    v1.0.1

---
+++ packages/taro-easy-ui-demo/src/pages/tabsAround/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {Container, Tabs} from "taro-easy-ui";
+import {View} from "@tarojs/components";
+
+
+export default class Index extends React.Component {
+    state: { current: number }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            current: 0
+        }
+    }
+
+    setCurrent(e: number) {
+        this.setState({
+            current: e
+        })
+    }
+
+    render() {
+        let {current} = this.state
+        return (
+            <Container space={24} style={{paddingBottom: '24rpx'}}>
+                <Tabs
+                    data={['标签一', '标签二', '标签三', '标签四', '标签五', '标签六', '标签七']}
+                    currentTabIndex={current}
+                    onTab={res => {
+                        this.setCurrent(res)
+                    }}
+                />
+                <View>内容{current + 1}</View>
+                <Tabs
+                    data={['标签一', '标签二', '标签三']}
+                    style={{display:'flex',justifyContent:'space-around'}}
+                />
+            </Container>
+        );
+    }
+}
